1374f3d03c2e08c58865d75cbf3187e735bb80e8
igyuwon <igyuwon60@gmail.com>
2024-05-07 09:41:14 +0900

    웹 스크래핑 통합

--- z05_webscrap_class/w0321/w0321_08.py
+++ z05_webscrap_class/w0321/w0321_08.py
@@ -2,40 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://finance.naver.com/sise/lastsearch2.naver"
-headers = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
-
-res = requests.get(url, headers=headers)
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
+res = requests.get(url,headers=headers)
 res.raise_for_status()
 soup = BeautifulSoup(res.text,"lxml")
-s_all = soup.find('div',{'class':'box_type_l'})
-tr_list = s_all.find_all('tr')
-# 타입 - 리스트 타입
-# print(len(tr_list))
-# print(type(tr_list))
-# with open('stock.html','w',encoding='utf8') as f:
-#     f.write(soup.prettify())
+# print(soup)
+s_all = soup.find("div",{"class":"box_type_l"})
+tr_list = s_all.find_all("tr")
+# 타입 - list타입
+print(len(tr_list))
+#----
+print("-"*50)
 for i in range(2,15):
     stock = tr_list[i]
-    td_list = stock.find_all('td')
-    if len(td_list) == 1:
-        continue
-    print('-'*60)
-    print('순위 : ',td_list[0].text)
-    print('종목명 : ',td_list[1].find('a').text)
-    print('검색비율 : ',td_list[2].text)
-    print('현재가 : ',td_list[3].text)
-    print('PER : ',td_list[10].text)
-    print('ROE : ',td_list[11].text)
-    print('-'*60)
+    td_list = stock.find_all("td")
+    if len(td_list) <= 1: continue
+    print("순위 : ",td_list[0].text)
+    print("종목명 : ",td_list[1].find("a").text)
+    print("검색비율 : ",td_list[2].text)
+    print("현재가 : ",td_list[3].text)
+    print("PER : ",td_list[10].text)
+    print("ROE : ",td_list[11].text)
     
-# print(samsung)
-# td_list = samsung.find_all('td')
-# print(td_list)
-# print('순위 : ',td_list[0].text)
-# print('종목명 : ',td_list[1].find('a').text)
-# print('검색비율 : ',td_list[2].text)
-# print('현재가 : ',td_list[3].text)
-# print(tr_list[2])
+    print("-"*50)
+#----
 
-# print(samsung)
+
+# with open("stock.html","w",encoding="utf8") as f:
+#     f.write(soup.prettify())
+
+# print(soup.prettify())
+
 
